100e8ec509d2a765032fa36aa6d67f7ace248db3
Tony Goncalves <tony@redlightsoft.com>
2016-03-30 15:08:17 +0100

    Added transcoding of higher bitrate mp3 to mpc

--- lowfi-mirror.rb
+++ lowfi-mirror.rb
@@ -1,12 +1,20 @@
+#!/usr/bin/env ruby
+
 require 'fileutils'
 
-COPY_TYPES = ['mp3', 'mpc', 'ogg', 'wma'].freeze
+
+MPCENC = 'mpcenc --silent'.freeze
+FFMPEG = (system('which ffmpeg > /dev/null 2>&1') ? 'ffmpeg' : 'avconv').freeze
+
+COPY_TYPES = %w(mpc ogg wma).freeze
 CONVERT_TYPES = {
   flac: :convert_flac_to_mpc,
   m4a:  :convert_alac_to_mpc,
-  wav:  :convert_wav_to_mpc
-}
-MPCENC = 'mpcenc --silent'.freeze
+  wav:  :convert_wav_to_mpc,
+  mp3:  :convert_mp3_to_mpc
+}.freeze
+
+
 
 def flac_bitrate(path)
   s = `file "#{path}"`
@@ -15,18 +23,37 @@ rescue
   999
 end
 
+def mp3_bitrate(path)
+  s = `file "#{path}"`
+  s[s.rindex(':')..-1].scan(/.*, ([0-9]*\.?[0-9]+) kbps,.*/).first.last.to_i
+rescue
+  999
+end
+
+
+
 def convert_flac_to_mpc(source, destination)
   sox = flac_bitrate(source) > 45 ? 'sox -twav - -twavpcm - rate -v 44100 |' : ''
   `flac -c -d "#{source}" | #{sox} #{MPCENC} - "#{destination}"`
 end
 
 def convert_alac_to_mpc(source, destination)
-  `avconv -i "#{source}" -f wav - | #{MPCENC} - "#{destination}"`
+  `#{FFMPEG} -i "#{source}" -f wav - | #{MPCENC} - "#{destination}"`
 end
 
 def convert_wav_to_mpc(source, destination)
   `#{MPCENC} "#{source}" "#{destination}"`
 end
+
+def convert_mp3_to_mpc(source, destination)
+  if mp3_bitrate(source) > 170
+    `mpg123 -w - -q "#{source}" | #{MPCENC} - "#{destination}"`
+  else
+    FileUtils.cp(source, "#{destination[0..-4]}mp3")
+  end
+end
+
+
 
 def file_tree(path)
   Dir["#{path}/**/*.{#{COPY_TYPES.join(',')},#{CONVERT_TYPES.keys.join(',')}}"]
@@ -35,6 +62,8 @@ end
 def destination_file(origin, destination, file)
   "#{destination}#{file[origin.length..-1]}"
 end
+
+
 
 def lossy_mirror(origin, destination)
   origin_files = file_tree(origin)
@@ -54,5 +83,6 @@ def lossy_mirror(origin, destination)
     end
   end
 end
+
 
 lossy_mirror(ARGV[0], ARGV[1]) if __FILE__ == $0
